ccfe04597124f849f7d64e8d8636578f262ae15d
him technology <130269588+him114514@users.noreply.github.com>
2025-04-06 09:10:44 +0800

    Delete frame_B2.py

--- frame_B2.py
+++
@@ -1,9 +0,0 @@
-import pyautogui as ui
-import random
-ui.FAILSAFE = False
-class shubiao:
-    def move(self,a,b):
-        ui.moveTo(random.randint(a,b), random.randint(a,b),duration=0)
-S=shubiao()        
-while True:
-    S.move(0,1000)
